7c46798bc3c27714ef1cc73c698cb18bc60b69f5
Phirathat <mous17zy@hotmail.com>
2020-12-25 10:22:47 +0700

    :sparkles: Add swagger

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ import helmet from 'helmet';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 
 import userRouter from '@src/modules/user/user.router';
 
@@ -17,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 app.use(cookieParser());
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/user', userRouter);
 
 export default app;
